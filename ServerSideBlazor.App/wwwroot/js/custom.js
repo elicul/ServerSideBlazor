756eeb4efabf80ca23b704e07cfac7ccf19011c5
elicul <enzo.licul@yahoo.com>
2019-01-21 15:42:17 +0100

    Update

--- ServerSideBlazor.App/wwwroot/js/custom.js
+++ ServerSideBlazor.App/wwwroot/js/custom.js
@@ -1,6 +1,15 @@
 window.navigationInit = () => {
     $(document).ready(function () {
         $('.sidenav').sidenav();
+        $(".dropdown-trigger").dropdown();
+    });
+
+    return true;
+};
+
+window.sidenavInit = () => {
+    $(document).ready(function(){
+        $('.collapsible').collapsible();
     });
 
     return true;
